1b50fda9edd87b4dcc68d266b2751f3438dba866
Xiaoyi Liu <lxy878@gmail.com>
2020-09-01 15:49:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function createEmployeeRecord(array){
+  const keys = ['firstName', 'familyName', 'title', 'payPerHour', 'timeInEvents', 'timeOutEvents'];
+  const newRecord = keys.reduce((records, key, index)=>{
+    records[key] = array.length > index ? array[index]:[];
+    return records;
+  }, {});
+  return newRecord;
+}
+
+
+function createEmployeeRecords(array2d){
+
+}
+
+function createTimeInEvent(){
+
+}
+
+function createTimeOutEvent(){
+
+}
+
+function hoursWorkedOnDate(){
+
+}
+
+function allWagesFor(){
+
+}
+
+function calculatePayroll(){
+
+}
