eb79287367a60aa95af8cdcd4418051b1a2572b6
Xiaoyi Liu <lxy878@gmail.com>
2020-09-02 04:36:03 +0000

    Done.

--- index.js
+++ index.js
@@ -11,25 +11,51 @@ function createEmployeeRecord(array){
 
 
 function createEmployeeRecords(array2d){
-
+  return array2d.map(array => createEmployeeRecord(array), []);
 }
 
-function createTimeInEvent(){
-
+function createTimeInEvent(record, date){
+  const timeIn = date.split(' ');
+  const newObj = {
+    'type': 'TimeIn',
+    'hour': parseInt(timeIn[1]),
+    'date': timeIn[0]
+  }
+  record.timeInEvents.push(newObj);
+  return record;
 }
 
-function createTimeOutEvent(){
-
+function createTimeOutEvent(record, date){
+  const timeOut = date.split(' ');
+  const newObj = {
+    'type': 'TimeOut',
+    'hour': parseInt(timeOut[1]),
+    'date': timeOut[0]
+  }
+  record.timeOutEvents.push(newObj);
+  return record;
 }
 
-function hoursWorkedOnDate(){
-
+function hoursWorkedOnDate(record, date){
+  const iTimeIn = record.timeInEvents.findIndex(timeIn => timeIn.date === date);
+  const iTimeOut = record.timeInEvents.findIndex(timeOut => timeOut.date === date);
+  return  (iTimeIn>=0 && iTimeOut>=0) ? (record.timeOutEvents[iTimeOut].hour - record.timeInEvents[iTimeIn].hour)/100:0;
 }
 
-function allWagesFor(){
-
+function wagesEarnedOnDate(record, date){
+  const hours = hoursWorkedOnDate(record, date);
+  return record.payPerHour * hours;
 }
 
-function calculatePayroll(){
+function allWagesFor(record){
+  const allDates = record.timeOutEvents.map(timeOut => timeOut.date, []);
+  return allDates.reduce((sum, date) => sum + wagesEarnedOnDate(record, date) , 0)
+}
 
+function findEmployeeByFirstName(records, firstName){
+  return records.find(record => record.firstName === firstName);
+}
+
+function calculatePayroll(records){
+  return records.reduce((sum, record) => sum+allWagesFor(record),0);
 }
